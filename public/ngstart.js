66baf1714b9c5824a803e5f4c7d9b37bf02a29df
shutiao <xuhaihua90@gmail.com>
2018-03-12 15:45:27 +0800

    Add ngController and $http Service

--- public/ngstart.js
+++ public/ngstart.js
@@ -1,3 +1,22 @@
 (function(){
-	var app = angular.module('testTarget', []);
+	var app = angular.module('test', []);
+	app.controller('ubbController', function(){
+		this.content = rules_config;
+	});
+
+	var rules_config = [{
+		name: "rule",
+		enabled: "false",
+		action: "block",
+		rule_id: 1,
+		condition: "path == '/form/moved/'"
+	}];
+
+	app.controller('ubbApiController', ['$http', function($http){
+		var ubbConf = this;
+		ubbConf.rules = [];
+		$http.get('/rules_config.json').success(function(data){
+			ubbConf.rules = data;
+		});
+	}]);
 })();
