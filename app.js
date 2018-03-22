2c7cc8c8aa6bb3350eaad8be0d6e0e24d184eae3
shutiao <xuhaihua90@gmail.com>
2018-03-22 22:47:21 +0800

    patch update index

--- app.js
+++ app.js
@@ -5,12 +5,30 @@ app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.sendFile('index.html');
+    res.redirect('/NGJS');
 });
 
 app.get('/abc', function(req, res){
     res.end('this is /abc');
 });
+
+app.get('/NGJS', function(req, res){
+    res.sendFile(__dirname + '/views/' + 'NGJS-UT.html');
+});
+
+app.get('/NG', function(req, res){
+    res.sendFile(__dirname + '/views/' + 'nav.html');
+});
+
+app.get('/React', function(req, res){
+    res.sendFile(__dirname + '/views/' + 'nav.html');
+});
+
+app.get('/VUE', function(req, res){
+    res.sendFile(__dirname + '/views/' + 'nav.html');
+});
+
+
 
 app.listen(3000);
 console.log('Express server started on port 3000');
