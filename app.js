b2715a3866c40e48ed7630325aac0b767e7b93e8
shutiao <xuhaihua90@gmail.com>
2018-03-26 00:03:30 +0800

    Add socket.io test page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-    app = express();
+var app = express();
+var server = require('http').createServer(app);
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
@@ -28,9 +29,29 @@ app.get('/VUE', function(req, res){
     res.sendFile(__dirname + '/views/' + 'nav.html');
 });
 
+app.get('/chattr', function(req, res){
+    res.sendFile(__dirname + '/views/' + 'chattr-console.html');
+})
 
 
-app.listen(3000);
-console.log('Express server started on port 3000');
+server.listen(3000);
+console.log('HTTP server started on port 3000');
 
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){
+    //client.emit('message', { hello: 'world'});
+    client.emit('connected', { status: true });
+    client.on('join', function(nickname){
+        // available both on the server and client
+        client.nickname = nickname;
+        console.log('Client ' + nickname + ' connected...');
+        client.broadcast.emit('message', nickname + ' joined the room');
+    });
+    client.on('message', function(message){
+        var nickname = client.nickname;
+        client.broadcast.emit('message', nickname + ": " + message);
+        client.emit('message', nickname + ": " + message);
+    });
+});
 
---
+++ public/chattr-console.js
@@ -0,0 +1,35 @@
+var socket = io.connect('http://localhost:3000');
+/*
+socket.on('message', function (data){
+    alert(data.hello);
+});
+*/
+
+var chat_input = document.getElementById('chat-input');
+var chat_console = document.getElementById('chat-console');
+
+socket.on('connected', function (isConnected){
+    if (isConnected.status){
+        document.getElementById('status').innerText = 'Connected To Chattr';
+        var nickname = prompt('What is your nickname?');
+        socket.emit('join', nickname);
+    }
+    else{
+        document.getElementById('status').innerText = 'Connecting';
+    }
+});
+
+document.getElementById('chat-form').onsubmit = function(e){
+    e.preventDefault();
+    socket.emit('message', chat_input.value);
+}
+
+socket.on('message', function(data){
+    insertMessage(data);
+});
+
+var insertMessage = function(message){
+    var new_message = document.createElement('span');
+    new_message.innerHTML = message + '<br/>';
+    chat_console.appendChild(new_message);
+}
