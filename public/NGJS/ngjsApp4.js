60740a64c2001316764c0641fb710da9301f2349
shutiao <xuhaihua90@gmail.com>
2018-04-02 19:58:43 +0800

    Add Form Cases Expecation

--- public/NGJS/ngjsApp4.js
+++ public/NGJS/ngjsApp4.js
@@ -3,4 +3,7 @@
 		angular.module("NGJS4").config(function($locationProvider) {
 		}).run(function($rootScope, $route, $location, $routeParams, $anchorScroll, ngProgress) {
 		});
+		testcase4.controller('testMarkup', function($scope){
+			$scope.AmIOK = 'I Am OK';
+		})
 })();
--- tests/spec.js
+++ tests/spec.js
@@ -1,7 +1,24 @@
 // spec.js
-describe('Protractor Demo App', function() {
+describe('Bian Demo App', function() {
     it('should have a title', function() {
       browser.get('http://localhost:3000/NGJS/main');
       expect(browser.getTitle()).toEqual('bian');
+    });
+  });
+
+  describe('AngularJS Single Page App', function() {
+    it('should load and compile correct template', function() {
+      browser.get('http://localhost:3000/NGJS/SPA');
+      element(by.linkText('Moby: Ch1')).click();
+      var content = element(by.css('[ng-view]')).getText();
+      expect(content).toMatch(/controller: ChapterController/);
+      expect(content).toMatch(/Book Id: Moby/);
+      expect(content).toMatch(/Chapter Id: 1/);
+
+      element(by.partialLinkText('Scarlet')).click();
+
+      content = element(by.css('[ng-view]')).getText();
+      expect(content).toMatch(/controller: BookController/);
+      expect(content).toMatch(/Book Id: Scarlet/);
     });
   });
