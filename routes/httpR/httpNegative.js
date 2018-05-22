34d6bea25d202b34d723d70411d8fd2d2af5e087
shutiao <xuhaihua90@gmail.com>
2018-05-22 12:54:31 +0800

    Implement HTTP-5-1

--- routes/httpR/httpNegative.js
+++ routes/httpR/httpNegative.js
@@ -6,6 +6,16 @@ httpNegative.route('/')
         res.render(__dirname + '/../../views/HTTP/negative.html');
     });
 
+httpNegative.route('/http-5-1')
+    .get(function(req, res){
+        res.writeHead(200);
+        res.write("<p>Dog is running.</p>");
+        setTimeout(function(){
+            res.write("<p>Dog is done.</p>");
+            //res.end();
+        }, 3000);
+    })
+
 httpNegative.route('/http-5-2')
     .get(function(req, res){
         res.render(__dirname + '/../../views/HTTP/negative/chrome_essential_protection_report.html')
