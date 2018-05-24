c83bac8e3ed200b89cdd8664cec202f10e2c1c81
shutiao <xuhaihua90@gmail.com>
2018-05-24 09:55:36 +0800

    Update HTTP-1-11 to Support Put Method

--- public/HTTP/tSuite-1-methods.js
+++ public/HTTP/tSuite-1-methods.js
@@ -62,11 +62,12 @@ $('#HTTP-1-10').on('click', '.ajax', function(event){
 
 $('#HTTP-1-11').on('click', 'button', function(event){
     event.preventDefault();
-    var enabled = $('#HTTP-1-11').find('input[name="enabled"]').is(':checked');
+    var enabled = $('#HTTP-1-11').find('select[name="httpMethod"]').val();
+    var httpMethpd = $('#HTTP-1-11').find('select').find(':selected').val();
     var reqHeaderField = $('#HTTP-1-11').find('input[name="reqHeaderField"]').val();
     var reqHeaderVal = $('#HTTP-1-11').find('input[name="reqHeaderVal"]').val();
     $.ajax({
-        method: 'POST',
+        method: httpMethpd || 'POST',
         url: '',
         dataType: "application/json",
         data: $('form[name="HTTP-1-11"]').serialize(),
