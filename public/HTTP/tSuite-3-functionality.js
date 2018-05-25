449b8f79878d4d0c26f5c93f6ccb4197b0b12abd
shutiao <xuhaihua90@gmail.com>
2018-05-25 10:28:16 +0800

    Udpate /HTTP/functionality to conform new coding style

--- public/HTTP/tSuite-3-functionality.js
+++ public/HTTP/tSuite-3-functionality.js
@@ -7,3 +7,23 @@ var HTTP_3_3 = function (){
     $('#HTTP-3-3').find('input[name="Method"]').val("POST");
     $('#HTTP-3-3').find('input[name="StatusCode"]').val("302");
 }
+
+$.fn.updateCaseID = function() {
+    this.each(function(){
+        var idArray = $(this).attr('zendao-case').split(',');
+        var idString = idArray.map(function(id, index){
+            if (index == 0){
+                return '#' + id;
+            }
+            else {
+                return ' #' + id;
+            }
+        })
+        caseIDHTML = ' (' + idString +') ';
+        $(this).find('h4').find('.caseID').text(caseIDHTML);
+        })
+}
+
+$(document).ready(function() {
+    $('.caseModule').updateCaseID();
+})
